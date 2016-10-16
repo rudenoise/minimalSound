a0eb413373dc19868c72825d2324dedfba0bb7ba
rudenoise <rudenoise@gmail.com>
2016-10-16 20:29:10 +0100

    add constants

--- square.c
+++ square.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define CYCLE_RADIUS 600
+#define AMP_MAX 255
+#define AMP_MIN 0
+
 int main(int argc, char** argv) {
+    // set start values for vars
     int tick = 0;
     bool max = false;
     // start unending loop
     while (1) {
-        if (tick == 600) {
+        if (tick == CYCLE_RADIUS) {
+            // half a cycle done, reset count
             tick = 0;
+            // reverse amplitude
             max = !max;
         }
+        // write out value
         if (max == true) {
-            putchar(255);
+            putchar(AMP_MAX);
         } else {
-            putchar(0);
+            putchar(AMP_MIN);
         }
+        // tick
         tick += 1;
     }
     return 0;
