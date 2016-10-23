f20478edfe4e46ef1134afcf79348a58849d9bd6
rudenoise <rudenoise@gmail.com>
2016-10-23 20:26:45 +0100

    bring draw-scripts into sync

--- bash/makeVisualiser.bash
+++ bash/makeVisualiser.bash
@@ -9,7 +9,7 @@ echo "gnuplot << EOF" >> _build/$1.bash
 echo "set terminal gif" >> _build/$1.bash
 echo "set output '_build/$1.gif'" >> _build/$1.bash
 echo "plot '-' using 1:2" >> _build/$1.bash
-_build/$1 | _build/testPlot >> _build/$1.bash
+_build/$1 300 | _build/testPlot >> _build/$1.bash
 echo "        e" >> _build/$1.bash
 echo "EOF" >> _build/$1.bash
 
