41383599b2fa0ea6d0f001b5a3ca78547d02a33a
rudenoise <rudenoise@gmail.com>
2016-10-28 18:30:06 +0100

    simplify sine wave

--- src/sine.c
+++ src/sine.c
@@ -10,8 +10,7 @@
 
 int main(int argc, char** argv) {
     // set start values for vars
-    bool rising = true, firstHalf = true;
-    int period, quaterPeriod, i, out;
+    int period, i, out;
     double stepRad;
 
     // get period
@@ -24,50 +23,25 @@ int main(int argc, char** argv) {
         period = 400;
     }
 
-    // setup an array to store amplitudes to 1/4 cycle
-    quaterPeriod = period / 4;
-    double sequence[quaterPeriod];
-
     // work out steps to make complete cycle
     // in radians
     stepRad = (2 * PI) / period;
 
-    // populate sequence
-    for (i = 0; i < quaterPeriod; i += 1) {
-        // calculate amplitude of opposite side
-        // using sine of step
-        // multiplied by radius (1/2 maximum amplitude)
-        sequence[i] = round(sin(stepRad * i) * ((AMP_MAX - 1) / 2));
-    }
-
-    // reset counter
+    // set counter
     i = 0;
 
     // generate wave infinitely
     while (1) {
 
-        if (firstHalf) {
-            out = (127 + sequence[i]);
-        } else {
-            out = (128 - sequence[i]);
+        if (i == period) {
+            i = 0;
         }
 
-        if (rising) {
-            i += 1;
-        } else {
-            i -= 1;
-        }
+        out = round(sin(stepRad * i) * ((AMP_MAX - 1) / 2));
 
-        if (i == 0) {
-            rising = true;
-            firstHalf = !firstHalf;
-        }
+        putchar(out + 127);
 
-        if (i == (quaterPeriod - 1)) {
-            rising = false;
-        }
-
-        putchar(out);
+        i += 1;
     }
 
     return 0;
